0fc6a9715d8310e4916ad657be2be65d28bb6307
Nina Aasen <nina.aasen@hig.no>
2015-09-21 16:28:31 +0200

    Added permissions in manifest, for external storage and record audio

--- app/src/main/java/com/nina/hig/chatter/MainActivity.java
+++ app/src/main/java/com/nina/hig/chatter/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -33,5 +34,9 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void recordButtonOnClick(View view) {
+
     }
 }
