307dd3826d914f6ee0844024af625989da5158a7
Nina Aasen <nina.aasen@hig.no>
2015-09-24 16:47:31 +0200

    Edit: Added code for record & stop

--- app/src/main/java/com/nina/hig/chatter/MainActivity.java
+++ app/src/main/java/com/nina/hig/chatter/MainActivity.java
@@ -1,42 +1,69 @@
 package com.nina.hig.chatter;
 
 import android.app.Activity;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
+import android.content.pm.PackageManager;
+import android.widget.Toast;
+
+import java.io.IOException;
+
 
 public class MainActivity extends Activity {
+
+    private static MediaRecorder mediaRecorder;
+
+    private static String audioFilePath;
+    private static ImageButton stopButton;
+    private static ImageButton playButton;
+    private static ImageButton recButton;
+
+    private boolean isRecording = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recButton = (ImageButton) findViewById(R.id.recButton);
+        playButton = (ImageButton) findViewById(R.id.playButton);
+        stopButton = (ImageButton) findViewById(R.id.stopButton);
+
+        audioFilePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/myaudio.3gp";
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void recordAudio(View view) throws IOException {
+        boolean isRecording = true;
+        stopButton.setEnabled(true);
+        playButton.setEnabled(false);
+        recButton.setEnabled(false);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        try {
+            mediaRecorder = new MediaRecorder();
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mediaRecorder.setOutputFile(audioFilePath);
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mediaRecorder.prepare();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        return super.onOptionsItemSelected(item);
-    }
+        mediaRecorder.start();
+        Toast.makeText(this, "recording", Toast.LENGTH_SHORT).show();
+        }
 
-    public void recordButtonOnClick(View view) {
-
-    }
-}
+    public void stopAudio(View view) throws IOException {
+           mediaRecorder.stop();
+           mediaRecorder.reset();
+           mediaRecorder.release();
+           mediaRecorder = null;
+           Toast.makeText(this, "audio recorded", Toast.LENGTH_SHORT).show();
+      }
+   }
